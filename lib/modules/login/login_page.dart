c9dd8f29b1fbbcbcac0862bb140c4f8ced39f5b6
hpzynha <larissa.nogueira.rocha@gmail.com>
2021-09-26 09:37:36 -0400

    Aula 3 Finalizada

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -4,6 +4,8 @@ import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
+import 'login_controller.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -59,7 +62,7 @@ class _LoginPageState extends State<LoginPage> {
                         const EdgeInsets.only(left: 40, right: 40, top: 40),
                     child: SocialLoginButton(
                       onTap: () {
-                        print("clicou");
+                        controller.googleSignIn(context);
                       },
                     ),
                   ),
