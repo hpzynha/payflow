bf8f53d93fd7894fe37aee26cac28804212abd14
hpzynha <larissa.nogueira.rocha@gmail.com>
2021-09-26 13:18:31 -0400

    Aula 4 ainda sem o erro

---
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  const BarcodeScannerPage({Key? key}) : super(key: key);
+
+  @override
+  _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Escaneie o código de barras do boleto",
+          style: TextStyles.buttonBackground,
+        ),
+        leading: BackButton(
+          color: AppColors.background,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
